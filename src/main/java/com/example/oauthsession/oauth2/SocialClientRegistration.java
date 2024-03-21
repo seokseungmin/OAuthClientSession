a0a80f438b987cd00433332a357173ce8f161461
SeokSeungMin <seoksmin@gmail.com>
2024-03-21 15:33:28 +0900

    SpringClientOAuth2Session

---
+++ src/main/java/com/example/oauthsession/oauth2/SocialClientRegistration.java
@@ -0,0 +1,43 @@
+package com.example.oauthsession.oauth2;
+
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.oidc.IdTokenClaimNames;
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class SocialClientRegistration {
+
+    public ClientRegistration naverClientRegistration() {
+
+        return ClientRegistration.withRegistrationId("naver")
+                .clientId("MV95HzDKoDe3wfUHU3qL")
+                .clientSecret("LpZFSrFf9M")
+                .redirectUri("http://localhost:8080/login/oauth2/code/naver")
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .scope("name", "email")
+                .authorizationUri("https://nid.naver.com/oauth2.0/authorize")
+                .tokenUri("https://nid.naver.com/oauth2.0/token")
+                .userInfoUri("https://openapi.naver.com/v1/nid/me")
+                .userNameAttributeName("response")
+                .build();
+    }
+
+    public ClientRegistration googleClientRegistration() {
+
+        return ClientRegistration.withRegistrationId("google")
+                .clientId("191317995709-1ca5uot79a0vtt88tsmr9giu06k5bnlv.apps.googleusercontent.com")
+                .clientSecret("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")
+                .redirectUri("http://localhost:8080/login/oauth2/code/google")
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .scope("profile", "email")
+                .authorizationUri("https://accounts.google.com/o/oauth2/v2/auth")
+                .tokenUri("https://www.googleapis.com/oauth2/v4/token")
+                .jwkSetUri("https://www.googleapis.com/oauth2/v3/certs")
+                .issuerUri("https://accounts.google.com")
+                .userInfoUri("https://www.googleapis.com/oauth2/v3/userinfo")
+                .userNameAttributeName(IdTokenClaimNames.SUB)
+                .build();
+    }
+}
