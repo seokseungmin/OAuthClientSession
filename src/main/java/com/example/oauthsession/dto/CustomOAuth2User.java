0c824eb1d770442ec8c0ecf648fde9588794be5f
SeokSeungMin <seoksmin@gmail.com>
2024-03-21 13:34:47 +0900

    SpringClientOAuth2Session

--- src/main/java/com/example/oauthsession/dto/CustomOAuth2User.java
+++ src/main/java/com/example/oauthsession/dto/CustomOAuth2User.java
@@ -43,4 +43,5 @@ public class CustomOAuth2User implements OAuth2User {
     public String getUsername() {
         return oAuth2Response.getProvider()+" "+oAuth2Response.getProviderId();
     }
+    //
 }
